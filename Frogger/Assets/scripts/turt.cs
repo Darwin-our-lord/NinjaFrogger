5680b8fd45c1d9d718571e4812e8877e78060056
CanadianEagle11 <whcp2020@gmail.com>
2026-08-13 13:36:07 +0200

    turt

--- Frogger/Assets/scripts/turt.cs
+++ Frogger/Assets/scripts/turt.cs
@@ -3,22 +3,21 @@ using UnityEngine;
 public class turt : MonoBehaviour
 {
     float bilx = 0;
-    int size = Random.Range(3, 7);
+    int size = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.localScale = new Vector3(size, transform.localScale.y, transform.localScale.z);
         bilx = transform.position.x;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (transform.position.x >= 10)
+        if (transform.position.x <= -10)
         {
             Destroy(gameObject);
         }
-        else { bilx = bilx + bil1.carspeed / 4; }
+        else { bilx = bilx -bil1.carspeed / 4; }
         transform.position = new Vector3(bilx, transform.position.y, transform.position.z);
     }
 }
