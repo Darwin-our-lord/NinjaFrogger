b32a6a075d68fa89e122c340e5c484fb6b1e78b4
CanadianEagle11 <whcp2020@gmail.com>
2026-08-13 12:59:26 +0200

    biller

---
+++ Frogger/Assets/scripts/bil 2.cs
@@ -0,0 +1,22 @@
+    using UnityEngine;
+
+public class bil2 : MonoBehaviour
+{
+
+    float bilx = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bilx = transform.position.x; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.x >= 10)
+        {
+            Destroy(gameObject);
+        } else { bilx = bilx + bil1.carspeed/4; }
+        transform.position = new Vector3(bilx, transform.position.y,transform.position.z);
+    }
+}
