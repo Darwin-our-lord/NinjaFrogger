72f31a5788edcd1531bcf3696a3f93a3b7bd8bd5
Darwin-our-lord <David@prebensen.dk>
2026-08-13 12:49:19 +0200

    goallll

---
+++ Frogger/Assets/scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public bool[] isGoalReached = new bool[5];
+    int score = 0;
+    float lastScoreTime = 0; //last time a score was counted
+    public void CheckForWin()
+    {
+        if(isGoalReached[0] && isGoalReached[1] && isGoalReached[2] && isGoalReached[3] && isGoalReached[4])
+        {
+            Debug.Log("All goals reached! You win!");
+        }
+    }
+    public void giveGoalScore()
+    {
+        score+= (int)(lastScoreTime-Time.time + 1000);
+    }
+
+
+}
