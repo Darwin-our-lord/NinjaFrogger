1abaf51cceff131f0b009cb9876c7e8307316b60
Darwin-our-lord <David@prebensen.dk>
2026-08-13 13:33:04 +0200

    slight fix on goal

--- Frogger/Assets/scripts/Goal.cs
+++ Frogger/Assets/scripts/Goal.cs
@@ -4,14 +4,20 @@ public class Goal : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
     [SerializeField] int goalIndex;
+
+    bool activeExit;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (!activeExit) return;
         if (collision.gameObject.CompareTag("Player"))
         {
             gameManager.isGoalReached[goalIndex] = true;
             gameManager.CheckForWin();
             gameManager.GiveGoalScore();
             collision.otherCollider.transform.position = new Vector3(0, 0, 0);
+            activeExit = false;
+            this.GetComponent<SpriteRenderer>().color = Color.hotPink;
             Debug.Log("Goal reached!");
         }
 
