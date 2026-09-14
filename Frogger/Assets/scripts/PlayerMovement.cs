1632ee7dd7b1206f60488315a5a4909ddf0f4104
Darwin-our-lord <David@prebensen.dk>
2026-09-14 10:36:18 +0200

    die

--- Frogger/Assets/scripts/PlayerMovement.cs
+++ Frogger/Assets/scripts/PlayerMovement.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Die() 
     {
-        
-    }
+        Debug.Log("Player dead");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
     }
+    
+
+
 }
