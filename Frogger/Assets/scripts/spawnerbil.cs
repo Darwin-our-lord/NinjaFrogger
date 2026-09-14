a1870de16d2332c6112813911b845eb4febba97e
CanadianEagle11 <whcp2020@gmail.com>
2026-09-14 13:05:48 +0200

    turt

--- Frogger/Assets/scripts/spawnerbil.cs
+++ Frogger/Assets/scripts/spawnerbil.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class spawnerbil : MonoBehaviour
 {
+    float turt_usyg = 0f;
     public GameObject zombiePrefab;
     public Transform spawnPoint;
     public float wait = 5;
@@ -12,10 +13,15 @@ public class spawnerbil : MonoBehaviour
     }
     void SpawnZombie()
     {
+        turt_usyg++;
+        if (turt_usyg >= 5) { turt_usyg = 0; }
         wait = 5;
         move = Random.Range(0, 3);
         wait = wait + move;
         Instantiate(zombiePrefab, spawnPoint.position, spawnPoint.rotation);
+        turt turle = zombiePrefab.GetComponent<turt>();
+        turle.turt_usymndlig = turt_usyg;
+
     }
     private void Update()
     {
